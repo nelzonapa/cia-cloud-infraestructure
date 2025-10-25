50c439053c272463e2551a55e835fb3f55e7f716
nelinux <jonel2000.fa@gmail.com>
2025-10-25 18:35:35 -0500

    Actualiza __main__.py agregando la creación de cluster GKE

--- cia-cloud-infrastructure/pulumi/base-stack/__main__.py
+++ cia-cloud-infrastructure/pulumi/base-stack/__main__.py
@@ -1,11 +1,17 @@
 import pulumi
-from components import networking
+from components import networking, cluster 
 
 # Crear la infraestructura de red
 network = networking.create_network()
+
+# Crear el cluster GKE
+gke_cluster = cluster.create_cluster(network)
 
 # Exportar información importante para referencia
 pulumi.export("vpc_name", network["vpc"].name)
 pulumi.export("subnet_name", network["subnet"].name)
 pulumi.export("subnet_region", network["subnet"].region)
 pulumi.export("vpc_id", network["vpc"].id)
+# nuevo
+pulumi.export("cluster_name", gke_cluster["cluster"].name)
+pulumi.export("cluster_endpoint", gke_cluster["cluster"].endpoint)
